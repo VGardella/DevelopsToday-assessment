33ed7235197552b3a8cc3db3fbd43dd1804ee3fb
Victoria Gardella <v.gardella.ruiz@gmail.com>
2024-09-05 11:57:35 -0300

    implement form and suspense components

--- car-dealer-app/src/app/page.js
+++ car-dealer-app/src/app/page.js
@@ -1,38 +1,33 @@
-import Dropdown from "./components/dropdown";
-import Link from "next/link";
+import Form from "./components/form";
+import LoadingMessage from "./components/loading";
+import { Suspense } from "react";
 
-export default async function Home({ params }) {
-  let carData;
+export default async function Home() {
+  let carList;
   try {
-    const data = await fetch("https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json").then((res) => res.json())
-    carData = data.Results
+    const data = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/vehicles/GetMakesForVehicleType/car?format=json`
+    ).then((res) => res.json());
+    carList = data.Results;
   } catch (err) {
     return err;
   }
 
-  const modelList = carData.map((item) => item = item.MakeName).sort();
   const startYear = 2015;
-  const currentYear = new Date().getFullYear()
-  const yearList = []
-  for (let year = startYear; year <= currentYear; year++) yearList.push(year)
+  const currentYear = new Date().getFullYear();
+  const yearList = [];
+  for (let year = startYear; year <= currentYear; year++) yearList.push(year);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="main-container">
       <div className="header">
         <h1>Header Text</h1>
       </div>
-      <div className="filter-container">
-        <form className="dropdown-form" action="" method="GET">
-          <div className="dropdown-container">
-            <Dropdown values={modelList} dropName="car" text="Choose a model" />
-            <Dropdown values={yearList} dropName="year" text="Choose a year" />
-          </div>
-          <button type="submit">
-            <Link href="/result/[makeId]/[year]" />
-          </button>
-        </form>
-      </div>
+      <Suspense fallback={<LoadingMessage />}>
+        <div className="filter-container">
+          <Form carList={carList} yearList={yearList} />
+        </div>
+      </Suspense>
     </main>
-    
   );
 }
