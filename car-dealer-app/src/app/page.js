c36500ec5bb7dfa5d35866be1c148253bc3267c9
Victoria Gardella <v.gardella.ruiz@gmail.com>
2024-09-05 14:50:41 -0300

    add paragraph and classes to elements

--- car-dealer-app/src/app/page.js
+++ car-dealer-app/src/app/page.js
@@ -19,15 +19,21 @@ export default async function Home() {
   for (let year = startYear; year <= currentYear; year++) yearList.push(year);
 
   return (
-    <main className="main-container">
-      <div className="header">
-        <h1>Header Text</h1>
-      </div>
-      <Suspense fallback={<LoadingMessage />}>
-        <div className="filter-container">
-          <Form carList={carList} yearList={yearList} />
+    <main class="h-full w-full max-w-3xl mx-auto flex flex-col justify-center items-center">
+      <div class="w-full">
+        <div class="py-10 text-center bg-pink-700 rounded-t-xl leading-10 text-white font-sans text-3xl font-semibold">
+          <h1>Welcome to Dev&apos;s car dealership!</h1>
         </div>
-      </Suspense>
+        <Suspense fallback={<LoadingMessage />}>
+          <div class="h-72 bg-white shadow-2xl rounded-b-xl p-10">
+            <p class="text-center font-semibold">
+              Need more information about any of our models? <br />
+              Choose from the menus bellow!
+            </p>
+            <Form carList={carList} yearList={yearList} />
+          </div>
+        </Suspense>
+      </div>
     </main>
   );
 }
