275df9eb2b766fb69d83431bfcc715dc52f85573
Victoria Gardella <v.gardella.ruiz@gmail.com>
2024-09-05 15:32:56 -0300

    fix suspense component position

--- car-dealer-app/src/app/page.js
+++ car-dealer-app/src/app/page.js
@@ -24,15 +24,15 @@ export default async function Home() {
         <div class="py-10 text-center bg-pink-700 rounded-t-xl leading-10 text-white font-sans text-3xl font-semibold">
           <h1>Welcome to Dev&apos;s car dealership!</h1>
         </div>
-        <Suspense fallback={<LoadingMessage />}>
-          <div class="h-72 bg-white shadow-2xl rounded-b-xl p-10">
+        <div class="h-72 bg-white shadow-2xl rounded-b-xl p-10">
+          <Suspense fallback={<LoadingMessage />}>
             <p class="text-center font-semibold">
               Need more information about any of our models? <br />
               Choose from the menus bellow!
             </p>
             <Form carList={carList} yearList={yearList} />
-          </div>
-        </Suspense>
+          </Suspense>
+        </div>
       </div>
     </main>
   );
