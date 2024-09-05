55cd430f459e18235ee927a27cfe9345a3226cb7
Victoria Gardella <v.gardella.ruiz@gmail.com>
2024-09-05 11:53:57 -0300

    lift up state

--- car-dealer-app/src/app/components/dropdown.js
+++ car-dealer-app/src/app/components/dropdown.js
@@ -1,25 +1,19 @@
-"use client"
-import { useState } from "react"
+"use client";
+import { useState } from "react";
 
-export default function Dropdown({ values, dropName, text }) {
-    const [ selectedValue, setSelectedValue ] = useState(null)
-
-    function handleClick(e) {
-        e.preventDefault();
-        setSelectedValue(e.target.value)
-    }
-
-    return(
-        <div id="main">
-            <select className="dropdown" name={dropName} onClick={handleClick} required>
-                <option value="">{text}</option>
-                {
-                    values.map((item) => {
-                        if (item)
-                        return <option key={item} value={item}>{item}</option>
-                    })
-                }
-            </select>
-        </div>
-    )
+export default function Dropdown({ values, dropName, text, onChange }) {
+  return (
+    <div id="main">
+      <select className="dropdown" name={dropName} onChange={onChange}>
+        <option value="">{text}</option>
+        {values.map((item) => {
+          return (
+            <option key={item} value={item}>
+              {item}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
 }
